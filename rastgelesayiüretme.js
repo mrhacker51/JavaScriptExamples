9bd0a87b27c490452e01e9ef49a011bcdb5b4cf0
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-28 10:33:09 +0300

    Create rastgelesayiüretme.js

---
+++ rastgelesayiüretme.js
@@ -0,0 +1,17 @@
+function sayiüret(){
+   return Math.ceil(Math.random() * 60)
+}
+
+
+sayiüret()
+
+
+var sayi1 = sayiüret()
+var sayi2 = sayiüret()
+var sayi3 = sayiüret()
+var sayi4 = sayiüret()
+var sayi5 = sayiüret()
+var sayi6 = sayiüret()
+
+
+console.log("Konsol : " + sayi1 + " " + sayi2 + " " + sayi3 + " " + sayi4 + " " + sayi5 + " " + sayi6)
