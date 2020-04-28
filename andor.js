7abe7eb6dfe618bb9b5d79ed07adc2ed18ff820e
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-28 21:23:10 +0300

    Update andor.js

--- andor.js
+++ andor.js
@@ -1,1 +1,13 @@
+var sayi1 = 30
+var sayi2 = 30
+var sayi3 = 30
 
+
+//if (sayi1 == sayi2 && sayi1 == sayi3){
+//    console.log("Eşittir")
+//}
+
+
+if (sayi1 == sayi2 && sayi1 == sayi3){
+    console.log("Eşittir")
+}
