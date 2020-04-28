dd8706eae6d0e2dac0a4fd1349fa89b26b3b383d
By Ethical Hacker <59092167+mrhacker51@users.noreply.github.com>
2020-04-28 22:34:39 +0300

    Update iciceforloop.js

--- iciceforloop.js
+++ iciceforloop.js
@@ -1,1 +1,11 @@
+function sayiüret(üstsayi = 51) { // Fonksiyon tanımladım
+    return Math.ceil(Math.random() * üstsayi) // fonksiyonun döndürecegi deger
+}
 
+
+for (var i = 1; i <= 8; i++) {  // döngü başlangıcı
+    console.log(i + " . Kolon") // kolonları ekrana yazdır
+    for (var j = 1; j <= 6; j++) { // içeri for döngüsü
+        console.log(sayiüret()) // yukarıdaki fonksiyonu döngünün içerisinde çagırıp ekrana yazdır
+    }
+}
